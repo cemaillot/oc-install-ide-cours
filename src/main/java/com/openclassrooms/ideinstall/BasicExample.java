e09b752ae07dba11f548ba71c2aa6bd4c11d4552
cemaillot <macetic@outlook.fr>
2023-05-07 14:10:04 +0200

    Correction des erreurs de syntaxe et des erreurs d'exécution 2

--- src/main/java/com/openclassrooms/ideinstall/BasicExample.java
+++ src/main/java/com/openclassrooms/ideinstall/BasicExample.java
@@ -1,10 +1,12 @@
 package com.openclassrooms.ideinstall;
 
-public class BasicExample {
-
-	public static void main(String[] args) {
+public class BasicExample 
+{
+	public static void main(String[] args) 
+	{
 		int i;
-		for(i=0; i<3; i++) {
+		for(i=0; i<3; i++) 
+		{
 			System.out.println("Hello bugs !");
 		}
 	}
